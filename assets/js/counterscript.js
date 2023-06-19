fad92a2b6350b5ce2a1a554a44ceaee9cb5c1517
Sassa19932 <_vodka_alla_cioccolata_@live.it>
2023-06-19 09:58:42 +0200

    up

---
+++ assets/js/counterscript.js
@@ -0,0 +1,14 @@
+// variables 
+
+let count = 0;
+
+function increment() {
+count++;
+document.getElementById("count").innerHTML = count;
+}
+
+function decrement() {
+  count--;
+  document.getElementById("count").innerHTML = count;
+}
+
