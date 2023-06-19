6e473b81e4702a63bc8dd53027739409293ac1cc
Sassa19932 <_vodka_alla_cioccolata_@live.it>
2023-06-19 19:24:37 +0200

    up

---
+++ assets/js/animations.js
@@ -0,0 +1,9 @@
+function changeBg() {
+    const images = [
+        "assets/img/backgroundimage.jpg",
+        "assets/img/backgroundimage2.jpg",
+        "assets/img/backgroundimage3.jpg",
+        "assets/img/backgroundimage4.jpg",
+    ];
+
+}
--- assets/js/counterscript.js
+++ assets/js/counterscript.js
@@ -1,14 +1,29 @@
 // variables 
+    const audioElement = new Audio("assets/sounds/click.wav");
+    let count = 0;
+    const decrement = document.getElementById('decrement');
+    const increment = document.getElementById('increment');
+    const reset = document.getElementById('reset');
+    let counter = document.getElementById('count');
 
-let count = 0;
+    //function decrement
+    
+    decrement.onclick = function(){
+    audioElement.play();
+    count--;
+    counter.innerHTML = count;
+    }
+    
 
-function increment() {
-count++;
-document.getElementById("count").innerHTML = count;
-}
+    reset.onclick = function(){
+      audioElement.play();
+      count = 0;
+      counter.innerHTML = count;
+      }
 
-function decrement() {
-  count--;
-  document.getElementById("count").innerHTML = count;
-}
-
+     //function increment
+   increment.onclick = function(){
+      audioElement.play();
+      count++;
+      counter.innerHTML = count;
+      }
