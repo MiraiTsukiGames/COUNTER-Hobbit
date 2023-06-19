51c559dce6c4903b4afdf44946754f31c6d42908
MiraiTsukiGames <118011618+MiraiTsukiGames@users.noreply.github.com>
2023-06-19 10:49:54 +0200

    Update counterscript.js

--- assets/js/counterscript.js
+++ assets/js/counterscript.js
@@ -1,8 +1,9 @@
 // variables 
 
 let count = 0;
-
+var sound = url("https://miraitsukigames.github.io/assets/sounds/click.m4a");
 function increment() {
+sound.play();
 count++;
 document.getElementById("count").innerHTML = count;
 }
