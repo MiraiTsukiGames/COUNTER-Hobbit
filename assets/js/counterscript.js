669a2b363a71dab158668991a2c9f5ad82c28d63
Sassa19932 <_vodka_alla_cioccolata_@live.it>
2023-06-19 19:30:25 +0200

    up

--- assets/js/counterscript.js
+++ assets/js/counterscript.js
@@ -6,7 +6,7 @@
     const reset = document.getElementById('reset');
     let counter = document.getElementById('count');
 
-<<<<<<< HEAD
+
     //function decrement
     
     decrement.onclick = function(){
@@ -21,20 +21,6 @@
       count = 0;
       counter.innerHTML = count;
       }
-=======
-let count = 0;
-var sound = new Audio url("https://miraitsukigames.github.io/Hobbit-Counter/assets/sounds/click.m4a");
-function increment() {
-sound.play();
-count++;
-document.getElementById("count").innerHTML = count;
-}
-
-function decrement() {
-  count--;
-  document.getElementById("count").innerHTML = count;
-}
->>>>>>> 792a9e7757559db119994dc781f9cc0d4edd3f32
 
      //function increment
    increment.onclick = function(){
